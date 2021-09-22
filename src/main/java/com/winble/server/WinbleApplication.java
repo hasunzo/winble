01d39a3f777258e6ef4e9f0858507996f9fdc944
hasunzo <gktjsdl470@gmail.com>
2021-09-22 21:51:06 +0900

    first commit

---
+++ src/main/java/com/winble/server/WinbleApplication.java
@@ -0,0 +1,13 @@
+package com.winble.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinbleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinbleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/winble/server/WinbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winble.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinbleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
