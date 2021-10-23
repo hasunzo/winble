d805f3e68dabc04789760133075c19181f8526b9
hasunzo <gktjsdl470@gmail.com>
2021-10-24 00:20:11 +0900

    update test

--- src/test/java/com/winble/server/WinbleApplicationTests.java
+++ src/test/java/com/winble/server/WinbleApplicationTests.java
@@ -3,11 +3,16 @@ package com.winble.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @SpringBootTest
 class WinbleApplicationTests {
 
     @Test
+    // ci 테스트를 위한 임시 테스트 코드
     void contextLoads() {
+        assertThat(1, is(1));
     }
 
 }
