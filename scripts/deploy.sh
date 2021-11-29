77feed9776f489c11206ebfc7d92bf9624b34cd8
hasunzo <gktjsdl470@gmail.com>
2021-11-29 16:12:59 +0900

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,5 +32,5 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/winble/properties/application-jwt.yml \
+    -Dspring.config.location=classpath:/application.yml,/home/ec2-user/winble/properties/application-jwt.yml,/home/ec2-user/winble/properties/application-oauth.yml \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
