2cf991ad02dd3cc6a953630090d2988d955ba685
hasunzo <gktjsdl470@gmail.com>
2022-01-07 23:30:11 +0900

    update ExceptionAdvice (#17)

--- src/main/java/com/winble/server/common/exception/ExceptionAdvice.java
+++ src/main/java/com/winble/server/common/exception/ExceptionAdvice.java
@@ -5,11 +5,14 @@ import com.winble.server.common.response.CommonResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 // Exception 발생시 ExceptionAdvice 에서 처리
 @Slf4j
@@ -17,15 +20,8 @@ import javax.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
-    private final ResponseService responseService;
-
-    // 그 외 Exception 발생시 처리하는 defalutException
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    protected CommonResult defaultException(HttpServletRequest request, Exception e) {
-        log.info(String.valueOf(e.getStackTrace()));
-        log.info(e.toString());
-        log.info(String.valueOf(e.getCause()));
-        return responseService.getFailResult(-9999, "알 수 없는 오류가 발생하였습니다.");
+    @ExceptionHandler(BizException.class)
+    public ResponseEntity<ErrorResponse> catchBizException(BizException exception) {
+        return new ResponseEntity<>(ErrorResponse.create(exception), exception.getHttpStatus());
     }
 }
